f54dc59c4703a9312d4d38a8988a93c45dbc1183
s_jaewoo <yureka1112@gmail.com>
2020-07-08 23:06:43 +0900

    fetch join - to fix N+1 and optimization

--- jpql/src/main/java/jpql/main.java
+++ jpql/src/main/java/jpql/main.java
@@ -27,30 +27,44 @@ public class main {
 
             Member member1 = new Member();
             member1.setUsername("member1");
-            member1.setTeam(team1);
+            member1.changeTeam(team1);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("member2");
-            member2.setTeam(team1);
+            member2.changeTeam(team1);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("member3");
-            member3.setTeam(team2);
+            member3.changeTeam(team2);
             em.persist(member3);
 
             em.flush();
             em.clear();
 
 
-            String query = "select m from Member m join fetch m.team";
+            //1. join fetch N:1
+            /*String query = "select m from Member m join fetch m.team";
             List<Member> resultList = em.createQuery(query, Member.class).getResultList();
-
 
             for (Member member : resultList) {
                 System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
-                // N + 1문제 발생
+            }*/
+
+            //2. join fetch 1:N, Collection type
+            /*String query = "select t from Team t join fetch t.members";
+            List<Team> resultList = em.createQuery(query, Team.class).getResultList();
+            for (Team team : resultList) {
+                System.out.println("team.getName() = " + team.getName() + " | members = " + team.getMembers().size());
+            }*/
+
+            //3. join fetch with distinct
+            // remove duplicate entities from resultList
+            String query = "select distinct t from Team t join fetch t.members";
+            List<Team> resultList = em.createQuery(query, Team.class).getResultList();
+            for (Team team : resultList) {
+                System.out.println("team.getName() = " + team.getName() + " | members = " + team.getMembers().size());
             }
 
             tx.commit();

3fc5a5d10d81f7d58463c34f91dcda8ac995569c
s_jaewoo <yureka1112@gmail.com>
2020-07-20 23:40:12 +0900

    study on spring data common and custom repository interface

---
+++ spring-data-jpa/src/main/java/me/devOwner/CommentRepository.java
@@ -0,0 +1,5 @@
+package me.devOwner;
+
+public interface CommentRepository extends MyRepository<Comment, Long> {
+
+}
--- spring-data-jpa/src/main/java/me/devOwner/PostRepository.java
+++ spring-data-jpa/src/main/java/me/devOwner/PostRepository.java
@@ -1,5 +1,7 @@
 package me.devOwner;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
@@ -20,4 +22,7 @@ public interface PostRepository extends JpaRepository<Post, Long> {
         return em.createQuery("select p from Post p", Post.class).getResultList();
     }*/
 
+    Page<Post> findByTitleContains(String title, Pageable pageable);
+
+    long countByTitleContains(String title);
 }

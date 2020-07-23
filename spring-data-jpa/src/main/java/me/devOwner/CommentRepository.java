b28d177c1ecaad96366e630c561a0bdf23697c24
s_jaewoo <yureka1112@gmail.com>
2020-07-23 23:06:43 +0900

    create query in spring data jpa

--- spring-data-jpa/src/main/java/me/devOwner/CommentRepository.java
+++ spring-data-jpa/src/main/java/me/devOwner/CommentRepository.java
@@ -1,5 +1,13 @@
 package me.devOwner;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface CommentRepository extends MyRepository<Comment, Long> {
 
+    List<Comment> findByCommentContains(String keyword);
+
+    Page<Comment> findByLikeCountGreaterThanAndPost(int likeCount, Post post, Pageable pageable);
 }
